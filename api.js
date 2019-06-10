1a44f6957d7366e23f5ca6a42c34ed59975e1562
Nick Moreton <nickmoreton@MacBook-Pro-3.local>
2019-06-10 10:18:16 +0100

    Fix crash issue

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 var request = require('request');
 
-module.exports = function(req, res, next) {
+module.exports = function (req, res, next) {
   var id = req.query.id,
     sheet = req.query.sheet || 1,
     query = req.query.q || '',
@@ -9,54 +9,56 @@ module.exports = function(req, res, next) {
     showColumns = req.query.columns || true,
     url = 'https://spreadsheets.google.com/feeds/list/' + id + '/' + sheet + '/public/values?alt=json';
 
-  request(url, function(error, response, body) {
-    if(!error && response.statusCode === 200) {
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
       var data = JSON.parse(response.body);
       var responseObj = {};
       var rows = [];
       var columns = {};
-      for(var i = 0; i < data.feed.entry.length; i++) {
-        var entry = data.feed.entry[i];
-        var keys = Object.keys(entry);
-        var newRow = {};
-        var queried = false;
-        for(var j = 0; j < keys.length; j++) {
-          var gsxCheck = keys[j].indexOf('gsx$');
-          if(gsxCheck > -1) {
-            var key = keys[j];
-            var name = key.substring(4);
-            var content = entry[key];
-            var value = content.$t;
-            if(value.toLowerCase().indexOf(query.toLowerCase()) > -1) {
-              queried = true;
-            }
-            if(useIntegers === true && !isNaN(value)) {
-              value = Number(value);
-            }
-            newRow[name] = value;
-            if(queried === true) {
-              if(!columns.hasOwnProperty(name)) {
-                columns[name] = [];
-                columns[name].push(value);
-              } else {
-                columns[name].push(value);
+      if (data && data.feed && data.feed.entry) {
+        for (var i = 0; i < data.feed.entry.length; i++) {
+          var entry = data.feed.entry[i];
+          var keys = Object.keys(entry);
+          var newRow = {};
+          var queried = false;
+          for (var j = 0; j < keys.length; j++) {
+            var gsxCheck = keys[j].indexOf('gsx$');
+            if (gsxCheck > -1) {
+              var key = keys[j];
+              var name = key.substring(4);
+              var content = entry[key];
+              var value = content.$t;
+              if (value.toLowerCase().indexOf(query.toLowerCase()) > -1) {
+                queried = true;
+              }
+              if (useIntegers === true && !isNaN(value)) {
+                value = Number(value);
+              }
+              newRow[name] = value;
+              if (queried === true) {
+                if (!columns.hasOwnProperty(name)) {
+                  columns[name] = [];
+                  columns[name].push(value);
+                } else {
+                  columns[name].push(value);
+                }
               }
             }
           }
+          if (queried === true) {
+            rows.push(newRow);
+          }
         }
-        if(queried === true) {
-          rows.push(newRow);
+        if (showColumns === true) {
+          responseObj['columns'] = columns;
         }
+        if (showRows === true) {
+          responseObj['rows'] = rows;
+        }
+        return res.status(200).json(responseObj);
+      } else {
+        return res.status(response.statusCode).json(error);
       }
-      if(showColumns === true) {
-        responseObj['columns'] = columns;
-      }
-      if(showRows === true) {
-        responseObj['rows'] = rows;
-      }
-      return res.status(200).json(responseObj);
-    } else {
-      return res.status(response.statusCode).json(error);
     }
   });
 };
